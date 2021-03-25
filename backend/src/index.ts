4b7666249f5a2df5e0bd6c300121797da5920361
Bryan Aafjes <432731@student.fontys.nl>
2021-03-25 09:32:43 +0100

    Separated data to insert

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
+import { Bolus2 } from "./bolus2";
 import {Bolus} from "./entity/Bolus";
 
 createConnection().then(async connection => {
-    const bolus = new Bolus();
-    bolus.weight = "90";
+    /*const bolus = new Bolus();
+    bolus.weight = "50";
     bolus.carbs = "70";
     await connection.manager.save(bolus);
-    console.log("Saved a new patiënt with weight: " + bolus.weight + " and carbs " + bolus.carbs);
+    console.log("Saved a new patiënt with weight: " + bolus.weight + " and carbs " + bolus.carbs);*/
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    await Bolus2(50, 50).catch((err) => { console.log(err);   });
 
 }).catch(error => console.log(error));
