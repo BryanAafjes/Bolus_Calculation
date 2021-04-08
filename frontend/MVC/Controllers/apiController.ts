b883106c41774f7feba523003bb44ac514054696
Bryan Aafjes <432731@student.fontys.nl>
2021-04-08 09:23:39 +0200

    Cleaned up frontend code

--- frontend/MVC/Controllers/apiController.ts
+++ frontend/MVC/Controllers/apiController.ts
@@ -1,7 +1,6 @@
 export class api {
     static async sendCalculationToAPI(weight: number, carbDose: number): Promise < boolean > {
         const date = new Date().toLocaleString();
-        //console.log(date.toISOString());
         const json = JSON.stringify({"weight":weight,"carbDose":carbDose,"calculationDateTime":date});
 
         const myHeaders = new Headers();
@@ -16,53 +15,25 @@ export class api {
         });
 
         if (response.ok) {
-            //console.log("Post created!")
             return true;
         } else {
-            //console.log("Post failed!")
             return false;
         }
     }
 
-    static async getCalculationFromApi() {
-    
+    static async getCalculationFromApi(): Promise < string > {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         myHeaders.append("Connection", "keep-alive");
         myHeaders.append("timeout", "5000");
-        let res;
-        //let data;
-    
+
         const response = await fetch("http://localhost:8000/api/getcalculation", {
            method: 'GET',
            headers: myHeaders,
         });
 
-        const data = await response.json().catch(error => console.log('ERROR'));
-        console.log(data, "howdy");
+        const data = await response.json().catch(error => console.log(error));
+        console.log(data);
         return data;
-                
-            
-       /*await fetch("http://localhost:8000/api/getcalculation", {
-            method: 'GET',
-            headers: myHeaders,
-            
-        })
-        .then(res => {
-            if(res.ok)
-            {
-                console.log("SUCESS");
-                res.json().then(data => {console.log(data); })
-            } else {
-                console.log("FAIL");
-            }
-            
-                       
-        })
-        .catch(error => console.log('ERROR'))
-        */
-        
     }
-
-    
 }
