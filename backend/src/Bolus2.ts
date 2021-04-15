2a5c4df2b1751d6000a41d721a3f4afa19ddef00
Albion <461340@student.fontys.nl>
2021-04-15 17:38:10 +0200

    Fixed calculationtime

--- backend/src/Bolus2.ts
+++ backend/src/Bolus2.ts
@@ -4,11 +4,12 @@ import { Bolus } from "./entity/Bolus"
 export const Bolus2 = async (Weight, Carbs, CalculationTime) => {
   const BolusRepo = getRepository(Bolus);
 
+  CalculationTime = new Date().toISOString();
+
   const bolus = BolusRepo.create({weight: Weight, carbs: Carbs, calculationTime: CalculationTime})
   await BolusRepo.save(bolus).catch((err) => {
       console.log(err);
   });
-
   console.log("saved carbs: ", bolus.carbs, "saved weight: ", bolus.weight, " Time of calculation: ", bolus.calculationTime)
 }
 
