7e6a2e9774533f7ca7ec6af238064be8014826a4
Bryan Aafjes <432731@student.fontys.nl>
2021-04-22 10:36:32 +0200

    Restructured getting inputs for calculation

---
+++ frontend/js/Models/userModel.js
@@ -0,0 +1,11 @@
+export var UserRole;
+(function (UserRole) {
+    UserRole["PATIENT"] = "Patient";
+    UserRole["GP"] = "Gp";
+})(UserRole || (UserRole = {}));
+var userModel = /** @class */ (function () {
+    function userModel() {
+    }
+    return userModel;
+}());
+export { userModel };
