9265aaf7a044d6f16a83342c85d8ea6c29955bb9
Quint Legius <quint.legius@iqfx.nl>
2021-04-24 15:14:02 +0200

    Fixed password hashing

--- backend/src/api/user/hashingHelper.ts
+++ backend/src/api/user/hashingHelper.ts
@@ -1,23 +1,18 @@
-// import * as bcrypt from 'bcrypt';
-// const saltRounds = 10; // should be 10
-// //const myPlaintextPassword = 's0/\/\P4$$w0rD';
+import * as bcrypt from 'bcrypt';
+import { resolve } from 'url';
+const saltRounds = 10; // should be 10
+//const myPlaintextPassword = 's0/\/\P4$$w0rD';
 
-// export class hashingHelper
-// {
-//     static hashPassword(passwordToHash: string):string
-//     {
-//         let hashedPassword:string;
-//         bcrypt.genSalt(saltRounds, function (err, salt) {
-//             bcrypt.hash(passwordToHash, salt, function (err, hash) {
-//                 hashedPassword = hash.toString();
-//             });
-//         });
-//         console.log(hashedPassword);
-//         return hashedPassword;
-//     }
+export class hashingHelper
+{
+    async hashPassword (passwordToHash: string)
+    {
+        const hashedPassword = await bcrypt.hash(passwordToHash, saltRounds);
+        return hashedPassword;
+    }
 
-//     static verifyHash(hashedPassword:string, passwordToCheck:string):boolean
-//     {
-//         return bcrypt.compareSync(passwordToCheck, hashedPassword);
-//     }
-// }
+    static verifyHash(hashedPassword:string, passwordToCheck:string):boolean
+    {
+        return bcrypt.compareSync(passwordToCheck, hashedPassword);
+    }
+}
