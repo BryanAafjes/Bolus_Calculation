de4a37cb8735832e003f3cf7fc3a9b5aedc11438
Bryan Aafjes <432731@student.fontys.nl>
2021-05-13 16:55:17 +0200

    Semi working ui changes and work in progress on login

--- backend/src/User.ts
+++ backend/src/User.ts
@@ -8,22 +8,36 @@ export const UserFunction = async (Username, Email, Password, Role, Created, Upd
   Created = new Date().toISOString();
   Updated = new Date().toISOString();
   const hashedPassword = await helper.hashPassword(Password);
+  //const selectedUser:User = await SelectUser(Email);
+
+  if(await SelectUser(Email) != null)
+  {
+    console.log("Duplicate entry for email");
+    throw new Error("Duplicate entry for email");
+  }
 
   const UserData = UserRepo.create({username: Username, email: Email, password: hashedPassword, role: Role, created_at: Created, updated_at: Updated})
   await UserRepo.save(UserData).catch((err) => {
       console.log(err);
   });
-
   console.log("username: ", UserData.username, "email: ", UserData.email, " password: ", UserData.password, " rol: ",UserData.role, " created: ",UserData.created_at, " updated: ",UserData.updated_at)
+
 }
 
-export const SelectUser = async (Email, Password) : Promise <Boolean> => {
+export const SelectUser = async (Email) : Promise <User> => {
   const UserRepo = getRepository(User);
   const helper = new hashingHelper();
   const UserData =  await UserRepo.find({where: {email: Email}}).catch((err) => {
     console.log(err);
     return false;
   });
+  return UserData[0];
+}
 
-  return await helper.verifyHash(UserData[0].password, Password);
+export const VerifyUserLogin = async (email:string, Password:string) : Promise <Boolean> => {
+  const UserRepo = getRepository(User);
+  const helper = new hashingHelper();
+  const UserData = await SelectUser(email);
+  console.log("jemoeder");
+  return await helper.verifyHash(UserData.password, Password);
 }
