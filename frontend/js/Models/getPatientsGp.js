6d4b2f7778a3f7449a6bd20fa8c4a4f6edc00455
Bryan Aafjes <432731@student.fontys.nl>
2021-06-10 15:18:29 +0200

    Made graphs for each patient belonging to GP

--- frontend/js/Models/getPatientsGp.js
+++ frontend/js/Models/getPatientsGp.js
@@ -36,6 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 };
 import { User } from "../../js/Controllers/userController.js";
 import { cookieHelper } from "../../js/Logic/cookieHelper.js";
+import { Chart, ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController, LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Decimation, Filler, Legend, Title, Tooltip } from '../../node_modules/chart.js/dist/chart.esm.js';
 export function getPatientsList() {
     return __awaiter(this, void 0, void 0, function () {
         var gpId;
@@ -50,4 +51,79 @@ export function getPatientsList() {
             }
         });
     });
+}
+export function PatientChart(user) {
+    //const userId = cookieHelper.getCookie("id");
+    //const data = await api.getCalculationFromApi(Number(userId));
+    //Promise.resolve(data);
+    var data = user.calculations;
+    var labels = [];
+    var weights = [];
+    var carbsDose = [];
+    var grafiekElement = document.createElement("canvas");
+    document.getElementById("append-" + user.id).append(grafiekElement);
+    //document.getElementById("patientTable").innerHTML = "";
+    var i = 0;
+    data.forEach(function () {
+        labels = labels.concat(new Date(data[i].calculationTime).toLocaleString());
+        weights = weights.concat(data[i].weight);
+        carbsDose = carbsDose.concat(data[i].carbsDoseNumber);
+        //document.getElementById("boluslist").insertAdjacentHTML("beforeend", "ID: " + data[i].id + " Weight: " + data[i].weight + " Carb Dose: " + data[i].carbs + " Calculation Time: " + date + "<br>" + "<br>");
+        i++;
+    });
+    var grafiekData = {
+        labels: labels,
+        datasets: [{
+                data: carbsDose,
+                fill: false,
+                borderColor: 'rgb(52, 235, 158)',
+                backgroundColor: 'rgb(52, 235, 158)',
+                label: 'Carbdose (in units)'
+            }, {
+                data: weights,
+                fill: false,
+                borderColor: 'rgb(245, 0, 37)',
+                backgroundColor: 'rgb(245, 108, 108)',
+                label: 'Weights (in kg)',
+            }]
+    };
+    Chart.register(ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController, LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Decimation, Filler, Legend, Title, Tooltip);
+    var grafiek = new Chart(grafiekElement, {
+        type: 'line',
+        data: grafiekData,
+        options: {
+            scales: {
+                yAxes: {
+                    stacked: false,
+                    Ticks: {
+                        mirror: true,
+                    },
+                },
+                xAxes: {
+                    stacked: false,
+                    Ticks: {
+                        mirror: true
+                    },
+                    title: {
+                        display: true,
+                        text: 'Time of calculation',
+                        color: '#000000',
+                        font: {
+                            family: 'sans-serif',
+                            size: 20,
+                            style: 'normal',
+                            lineHeight: 1.2
+                        },
+                        padding: {
+                            top: 30,
+                            left: 0,
+                            right: 0,
+                            bottom: 0
+                        }
+                    }
+                }
+            }
+        },
+    });
+    return grafiek;
 }
